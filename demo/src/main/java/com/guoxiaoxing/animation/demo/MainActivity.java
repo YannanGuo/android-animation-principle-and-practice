5f771ed3e3e9f09e5f1f91214369431d5d451c4f
guoxiaoxing <guoxiaoxing@souche.com>
2016-09-29 17:59:08 +0800

    add view animation

--- demo/src/main/java/com/guoxiaoxing/animation/demo/MainActivity.java
+++ demo/src/main/java/com/guoxiaoxing/animation/demo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.guoxiaoxing.animation.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.guoxiaoxing.animation.demo.view.ViewAnimationActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.btn_view_animation)
+    Button mBtnViewAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mBtnViewAnimation.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.btn_view_animation:
+                startActivity(new Intent(MainActivity.this, ViewAnimationActivity.class));
+                break;
+        }
     }
 }
---
+++ demo/src/main/java/com/guoxiaoxing/animation/demo/view/ViewAnimationActivity.java
@@ -0,0 +1,15 @@
+package com.guoxiaoxing.animation.demo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.guoxiaoxing.animation.demo.R;
+
+public class ViewAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_animation);
+    }
+}
